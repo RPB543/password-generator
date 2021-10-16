ca397b9a2fb7ead7cd93ac8e950a9f0c90197c8d
Becca Byrne <aggiebec06@gmail.com>
2021-10-16 18:39:38 -0500

    added characters in password

--- script.js
+++ script.js
@@ -13,33 +13,45 @@ var lowerCase = ['a','b','c','d', 'e','f','g','h','i','j','k','l','m','n','o','p
 var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
-
-//Start function
 // Generate password
 function generatePassword ()  {
-  // Ask for user input
+
+  // Ask for length of passwrod
 passwordLength = prompt("How long would you like your password? Please choose between 8 and 128 characters.");
-console.log("Password length is " + passwordLength + ".");
+    console.log("Password length is " + passwordLength + ".");
 
-if(passwordLength === "") {
-  window.alert("Input required.  Please choose a password length between 8 and 128.");
-}
-
- else if (passwordLength < 8 && passwordLength > 128);{
+if (passwordLength < 8 || passwordLength > 128) {
 passwordLength = prompt("Password must be between 8 and 128. Please choose again.");
   console.log("Password length " + passwordLength);
+  alert("Your password will have " + passwordLength + " characters.");
+
+} else if (passwordLength >= 8 && passwordLength <= 128) {
+  console.log("Password length " + passwordLength);
+  alert("Your password will have " + passwordLength + " characters.");
+
+}else {console.log("Password length " + passwordLength);
 }
-  {
-  confirmLower = confirm("Would you like to include a lower case letter?");
-  console.log("Lower case letter is " + confirmLower);
-  confirmUpper = confirm("Would you like to include an upper case letter?");
+
+//Confirm characters in password
+var passwordChars =[]
+
+  confirmLower = prompt("Please enter a lower case letter, if you would like to include one.");
+    passwordChars = passwordChars.concat(lowerCase);
+    console.log("Lower case letter is " + confirmLower);
+  
+
+  confirmUpper = prompt("Please enter a upper case letter, if you would like to include one.");
+  passwordChars = passwordChars.concat(upperCase);
   console.log("Upper case letter is " + confirmUpper);
-  confirmNumber = confirm("Would you like to include a number?");
+
+  confirmNumber = prompt("Please enter a number, if you would like to include one.");
+  passwordChars = passwordChars.concat(numbers);
   console.log("Number is " + confirmNumber);
-  confirmSymbol = confirm("Would you like to include a symbol?");
+
+  confirmSymbol = prompt("Would you like to include a symbol?");
+  passwordChars = passwordChars.concat(symbols);
   console.log("Symbol is " + confirmSymbol);
 };
-}
 
 //for(var 1 = 0; i < passwordLength, i++) {
   //console.log(password)
