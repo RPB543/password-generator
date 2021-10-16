37d44d731b37b4c4d48a7f89c961eaa3bb3dcae7
Becca Byrne <aggiebec06@gmail.com>
2021-10-16 09:33:55 -0500

    added logic for password length

--- script.js
+++ script.js
@@ -1,6 +1,116 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate password
+function generatePassword () {
+    //do stuff
+    const totalLength = prompt('How many characters would you like to use?');
+    if(totalLength >= 8 && totalLength <= 128) {
+      alert(`Your new password will be ${totalLength} characters long.`);
+    }
+}
+
+//Upper case letters
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+//Lower case letters
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+//Symbols
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+//Numbers
+var numbersCharacters = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0'
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
